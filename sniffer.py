1154b85fb67539f4bb56be76ece0d3cad5a6fdb7
Konstantin Bozhkov <konstantin.bozhkov@yahoo.com>
2019-01-08 00:36:23 +0000

    Upacking data from ICMP and initial code added for TCP

--- sniffer.py
+++ sniffer.py
@@ -4,7 +4,7 @@ import textwrap
 
 
 def main():
-    connection = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+    connection = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
     while True:
         raw_data, address = connection.recvfrom(65536)
         desc_mac, src_mac, ethernet_proto, data = ethernet_frame(raw_data)
@@ -24,6 +24,28 @@ def get_mac_addr(bytes_addr):
     mac_address = ':'.join(bytes_str).upper()
     return mac_address
 
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15 ) * 4
+
+    ttl, ip_protocol, src, target = struct.unpack('! 8x B B 2x 4s 4s',data[:20])
+    return version,header_length, ttl, ip_protocol, ipv4(src), ipv4(target), data[header_length:] # data[] starts from the end of the header length
+
+#127.0.0.0.1
+# Return  formatted IPv4 address
+def ipv4(addr):
+    return '.'.join(map(str,addr))
+
+
+
+# Unpack ICMP packets
+def  unpack_icmp(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+def unpack_tcp(data):
+    (source_port, destination_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L L H ',data[:14])
 
 main()
 
